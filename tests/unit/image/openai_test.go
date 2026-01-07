257255f191997fc14c9fcd5bd3136baa80790176
wilhuang <wilhuang@easyops.cn>
2026-01-07 15:17:14 +0800

    chore(): go lint

--- tests/unit/image/openai_test.go
+++ tests/unit/image/openai_test.go
@@ -44,7 +44,7 @@ func TestOpenAIClient_Generate(t *testing.T) {
 			},
 		}
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(resp)
+		_ = json.NewEncoder(w).Encode(resp)
 	}))
 	defer server.Close()
 
